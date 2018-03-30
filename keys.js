0f010b2f039321600932c124a44942e7307b2aab
AngelaF <ADFrys@users.noreply.github.com>
2018-03-29 20:50:24 -0500

    Update keys.

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY || keys.cusumer_key,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET || keys.consumer_secret,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY || keys.access_token,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || keys.access_token_secret
 };
